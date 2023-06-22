8fb8e861f929fa570888d2f40c00db985e8fb26d
Sumeyye Nalbat <126310441+sumiyye@users.noreply.github.com>
2023-06-22 22:22:50 +0300

    githuba gittiginden mein olmak icin satir ekleme

--- bioinfo_denem.py
+++ bioinfo_denem.py
@@ -1,4 +1,7 @@
 def say_hello():
     print('merhaba')
     print('yeniden merhaba')
+    print('yeniden merhaba')
     print('pycharma gidiyomu bi bak')
+    print('githuba gidiyomu bi bak')
+    print('emin olayim bi tekrar deneyeyim nasil gidiyor githuba ')
