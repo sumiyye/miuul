c02fff4020657948c3a901f739a3fe78bef1053e
Sumeyye Nalbat <126310441+sumiyye@users.noreply.github.com>
2023-06-26 20:03:13 +0300

    yeniden acildiginde yapilan degisiklik

--- bioinfo_denem.py
+++ bioinfo_denem.py
@@ -1,5 +1,4 @@
 def say_hello():
     print('merhaba')
-    print('yeniden merhaba')
-    print('pycharma gidiyomu bi bak')
-    print('githuba gidiyor mu deneme ')
+    print('yeniden merhaba ***')
+
